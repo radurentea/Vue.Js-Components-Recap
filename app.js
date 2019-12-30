4235ea41d74eb13cab369055bb34defa98efc27f
Radu Rentea <radu.rentea@gmail.com>
2019-12-30 11:58:29 +0200

    Pick Plan exercise

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-Vue.component('click-counter', {
-  template: '#click-counter-template',
-  data() {
-    return {
-      count: 0,
-    }
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  props: {
+    name: String,
   },
-})
+});
 
 var app = new Vue({
   el: '#app',
+  data: {
+    plans: ["The Single", "The Curious", "The Addict",],
+  }
 })
