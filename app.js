ef725cd9eb36313f1fb6e44399ad58cfea6ef69b
Radu Rentea <radu.rentea@gmail.com>
2019-12-30 13:51:59 +0200

    Add feature to be able to select only one plan

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ let PlanComponent = {
   template: '#plan-template',
   props: {
     name: String,
+    selectedPlan: {
+      type: String,
+    },
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name);
+    },
+  },
+  computed: {
+    isSelected() {
+      return this.name === this.selectedPlan;
+    },
   },
 };
 
@@ -10,7 +23,13 @@ let PlanPickerComponent = {
   data() {
     return {
       plans: ["The Single", "The Curious", "The Addict",],
+      selectedPlan: null,
     };
+  },
+  methods: {
+    selectPlan(payload) {
+      this.selectedPlan = payload;
+    }
   },
   components: {
     plan: PlanComponent,
